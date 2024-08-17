ab8d74d7deeb170567f2e10f9105b4c4b4c9d703
victorfeldhaus <victorm.feldhaus@gmail.com>
2024-08-17 17:55:31 -0300

    feat: create mysql server with compose

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,3 +1,12 @@
+import { log } from "console";
+import express from "express";
+
+const PORT = 3333
+
 export const server = () => {
-    
+    const app = express();
+
+    routes(app)
+
+    app.listen(PORT, () => { log(`Server is running in port ${PORT}`)})
 };
